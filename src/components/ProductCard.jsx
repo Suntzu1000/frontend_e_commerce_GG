5f707d7623d9d9c29b639e6789773965fb65e6ec
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-04-18 14:51:51 -0300

    Página Loja 50%

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ProductCard = () => {
+  let location = useLocation();
   return (
     <div className="col-3">
       <Link className="product-card position-relative">
--- src/pages/OurStore.jsx
+++ src/pages/OurStore.jsx
@@ -1,8 +1,13 @@
-import React from "react";
 import BreadCrumb from "../components/BreadCrump";
+import React, { useState } from "react";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
+import ProductCard from "../components/ProductCard";
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
+  
+
   return (
     <>
       <Meta title={"Lojão"} />
@@ -121,18 +126,131 @@ const OurStore = () => {
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Tags de Produtos</h3>
                 <div>
-                    <div className="product-tags d-flex flex-wrap align-items-center gap-10">
-                        <span className="badge bg-secondary rounded-3 py-2 px-3">
-                            Fones de Ouvido
-                        </span>
-                    </div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Fones de Ouvido
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Apple´s
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                  </div>
                 </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Produtos Aleatórios</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="Relógio"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Fones de Ouvido de cores variadas.</h5>
+                      <ReactStars
+                        count={5}
+                        value="3"
+                        size={24}
+                        edit="false"
+                        activeColor="#ffd700"
+                      />
+                      <b>R$300</b>
+                    </div>
+                  </div>
+
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="Relógio"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Fones de Ouvido de cores variadas.</h5>
+                      <ReactStars
+                        count={5}
+                        value="3"
+                        size={24}
+                        edit="false"
+                        activeColor="#ffd700"
+                      />
+                      <b>R$300</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block" style={{ width: "100px" }}>
+                      Ordenar:
+                    </p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual">Destaque</option>
+                      <option value="best-selling">Mais Vendidos</option>
+                      <option value="title-ascending">
+                        Ordem Alfabética, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Ordem A. Decrescente , Z-A
+                      </option>
+                      <option value="price-ascending">
+                        Preço, baixo para alto
+                      </option>
+                      <option value="price-descending">
+                        Price, auto para baixo
+                      </option>
+                      <option value="created-ascending">
+                        Data, antigo para novo
+                      </option>
+                      <option value="created-descending">
+                        Data, novo para antigo
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Produtos</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img onClick={() => {setGrid()}}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img onClick={() => {setGrid()}}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img onClick={() => {setGrid()}}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+
+                      <img onClick={() => {setGrid()}}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <ProductCard grid={grid} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
