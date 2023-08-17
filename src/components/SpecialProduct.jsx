57feb1e8763e6ae02ecae282e8fddb9e9fd6ec22
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-08-17 14:52:05 -0300

    Home 45%

--- src/components/SpecialProduct.jsx
+++ src/components/SpecialProduct.jsx
@@ -1,61 +1,66 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
 import { Link } from "react-router-dom";
-import watch from "../images/watch.jpg";
 
 const SpecialProduct = (props) => {
-  const { title, brand, totalrating, price, sold, quantity, id } = props;
+  const { title, brand, totalrating, price, sold, quantity, id, images } = props;
+  const formattedPrice = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
   return (
-    <>
-      <div className="col-6 mb-3">
-        <div className="special-product-card">
-          <div className="d-flex justify-content-between">
-            <div>
-              <img src={watch} className="img-fluid" alt="Relógio" />
-            </div>
-            <div className="special-product-content">
-              <h5 className="brand">{brand}</h5>
-              <h6 className="title">{title}</h6>
-              <ReactStars
-                count={5}
-                size={24}
-                value={totalrating}
-                edit={false}
-                activeColor="#ffd700"
-              />
-              <p className="price">
-                <span className="red-p">R${price}</span> &nbsp;{" "}
-                <strike>R$</strike>
+    <div className="col-6 mb-3">
+      <div className="special-product-card bg-white border border-gray-300 rounded shadow-md p-4">
+        <div className="flex justify-between">
+          <div>
+            <img src={images} className="img-fluid" alt="Relógio" />
+          </div>
+          <div className="special-product-content ml-4">
+            <h5 className="brand text-lg font-bold">{brand}</h5>
+            <h6 className="title text-base">{title}</h6>
+            <ReactStars
+              count={5}
+              size={24}
+              value={totalrating}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="price text-xl mt-2">
+              <span className="text-green-500">{formattedPrice}</span> &nbsp;
+            </p>
+            <div className="discount-till flex items-center mt-2">
+              <p className="mb-0">
+                <b>5 </b>days
               </p>
-              <div className="discount-till d-flex align-items-center gap-10">
-                <p className="mb-0">
-                  <b>5 </b>days
-                </p>
-                <div className="d-flex gap-10 align-items-center">
-                  <span className="badge rounded-circle p-3 bg-danger">1</span>:
-                  <span className="badge rounded-circle p-3 bg-danger">1</span>:
-                  <span className="badge rounded-circle p-3 bg-danger">1</span>
-                </div>
+              <div className="flex gap-2 items-center">
+                <span className="badge rounded-full p-3 bg-danger">1</span>:
+                <span className="badge rounded-full p-3 bg-danger">1</span>:
+                <span className="badge rounded-full p-3 bg-danger">1</span>
               </div>
-              <div className="prod-count my-3">
-                <p>Produtos: {quantity}</p>
-                <div className="progress">
-                  <div
-                    className="progress-bar"
-                    role="progressbar"
-                    style={{ width: quantity / quantity + sold * 100 + "%" }}
-                    aria-valuenow={quantity / quantity + sold * 100}
-                    aria-valuemin={quantity}
-                    aria-valuemax={sold + quantity}
-                  ></div>
-                </div>
-              </div>
-              <Link className="button" to={`/product/`+id}>Carrinho</Link>
             </div>
+            <div className="prod-count my-3">
+              <p>Produtos: {quantity}</p>
+              <div className="progress h-2 bg-gray-200 rounded">
+                <div
+                  className="progress-bar bg-indigo-600 rounded"
+                  role="progressbar"
+                  style={{ width: `${(quantity / (quantity + sold)) * 100}%` }}
+                  aria-valuenow={(quantity / (quantity + sold)) * 100}
+                  aria-valuemin={quantity}
+                  aria-valuemax={sold + quantity}
+                ></div>
+              </div>
+            </div>
+            <Link
+              className="button inline-block px-4 py-2 bg-indigo-600 text-white rounded mt-2"
+              to={`/product/${id}`}
+            >
+              Carrinho
+            </Link>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,6 @@ import BlogCard from "../components/BlogCard";
 import SpecialProduct from "../components/SpecialProduct";
 import Container from "../components/Container";
 import { services } from "../utils/Data";
-import famous from "../images/famous-1.webp";
-import famous2 from "../images/famous-2.webp";
-import famous3 from "../images/famous-3.webp";
-import famous4 from "../images/famous-4.webp";
 import brand from "../images/brand-01.png";
 import brand2 from "../images/brand-02.png";
 import brand3 from "../images/brand-03.png";
@@ -28,13 +24,21 @@ import cart from "../images/add-cart.svg";
 import { addWishList } from "../features/products/productsSlice";
 import watch2 from "../images/watch-1.avif";
 import ReactStars from "react-rating-stars-component";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const blogState = useSelector((state) => state?.blog?.blog);
   const productState = useSelector((state) => state.product.product);
-  const [displayCount, setDisplayCount] = useState(5);
+  const [displayCount, setDisplayCount] = useState(8);
+  const calculateInstallmentPrice = useCallback(
+    (price) => Math.round(price / 12),
+    []
+  );
 
   const addToWishList = (id) => {
     dispatch(addWishList(id));
@@ -55,46 +59,69 @@ const Home = () => {
 
   return (
     <>
-      <Container className="mx-auto px-4 sm:px-6 lg:px-8 py-5">
-        <div className="flex flex-wrap -m-4">
+      <Container class1="mx-auto px-4 sm:px-6 lg:px-8 py-5">
+        <Slider
+        className="flex flex-wrap -m-4"
+        slidesToShow={4}
+        slidesToScroll={1}
+        autoplay={false}
+        dots={false}
+        responsive={[
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+            },
+          },
+        ]}
+      >
           {productState &&
             productState?.map((item, ind) => {
-              if (item.tags === "popular") {
-                const formattedPrice = new Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                }).format(item.price);
-                return (
-                  <div className="lg:w-[25%] p-4" key={ind}>
-                    <div className="h-full bg-white p-8 rounded shadow-lg flex flex-col justify-between">
-                      <Link
-                        to={`/product/${item?._id}`}
-                        className="border-0 bg-transparent"
-                      >
-                        <img
-                          src={item.images[0].url}
-                          className="w-[100%]  h-auto rounded-t hover:scale-125 transition duration-300"
-                          alt="Produtos Eletrônicos"
-                        />
+              const formattedPrice = new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(item.price);
+              return (
+                <div className="lg:w-100 p-4 h-auto bg-white" key={ind}>
+                  <div className="w-100  h-full bg-white p-8 rounded shadow-lg flex flex-col justify-between">
+                    <Link
+                      to={`/product/${item?._id}`}
+                      className="border-0 bg-transparent"
+                    >
+                      <img
+                        src={item.images[0]?.url}
+                        className="w-[100%]  h-auto rounded-t hover:scale-125 transition duration-300"
+                        alt="Produtos Eletrônicos"
+                      />
+                    </Link>
+                    <div className="mt-6">
+                      <h2 className="text-2xl font-semibold text-gray-900">
+                        {item.title}
+                      </h2>
+                      <p className="text-xl font-semibold text-green-500">
+                        {formattedPrice}
+                      </p>
+                      <Link className="mt-4 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Comprar
                       </Link>
-                      <div className="mt-6">
-                        <h2 className="text-2xl font-semibold text-gray-900">
-                          {item.title}
-                        </h2>
-                        <p className="text-xl font-semibold text-green-500">
-                          {formattedPrice}
-                        </p>
-                        <Link className="mt-4 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                          Comprar
-                        </Link>
-                      </div>
                     </div>
                   </div>
-                );
-              }
-              return true;
+                </div>
+              );
             })}
-        </div>
+        </Slider>
       </Container>
 
       <Container class1="home-wrapper-2 py-5">
@@ -184,48 +211,67 @@ const Home = () => {
         </div>
       </Container>
 
-      <Container class1="famous-wrapper py-5 home-wrapper-2">
-        {productState &&
-          productState.slice(0, displayCount).map((product, index) => (
-            <div
-              key={index}
-              className="flex flex-col overflow-hidden rounded shadow-lg m-6 cursor-pointer transform hover:scale-105 transition-transform duration-200"
-            >
-              {" "}
-              <Link
-                to={`/product/${product?._id}`}
-                className="border-0 bg-transparent"
-              >
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-48 w-full object-cover"
-                    src={
-                      product.images[0]?.url ? product.images[0]?.url : watch2
-                    }
-                    alt={product.title}
-                  />
+      <Container class1=" py-5 home-wrapper-2 ">
+        <div className="grid lg:grid-cols-4 sm:grid-cols-1">
+          {productState &&
+            productState.slice(0, displayCount).map((product, index) => {
+              const formattedPrice = new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.price);
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col overflow-hidden m-6 rounded shadow-lg  cursor-pointer transform hover:scale-105 transition-transform duration-200 w-[90%] h-auto "
+                >
+                  {" "}
+                  <Link
+                    to={`/product/${product?._id}`}
+                    className="border-0 bg-transparent"
+                  >
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-auto w-100 object-cover"
+                        src={
+                          product.images[0]?.url
+                            ? product.images[0]?.url
+                            : watch2
+                        }
+                        alt={product.title}
+                      />
+                    </div>
+                  </Link>
+                  <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                    <div className="flex-1">
+                      <p className="text-sm font-medium text-indigo-600">
+                        {product.category}
+                      </p>
+                      <h5 className="mt-2 text-xl font-semibold text-gray-900">
+                        {product.title}
+                      </h5>
+                      <p
+                        className="mt-3 text-base text-gray-500"
+                        dangerouslySetInnerHTML={{
+                          __html: product.description,
+                        }}
+                      ></p>
+                      <p className="mt-3 text-xl font-semibold ">
+                        De{" "}
+                        <span className="text-green-500">{formattedPrice}</span>{" "}
+                        ou{" "}
+                        <span className="text-green-500">
+                          R${calculateInstallmentPrice(product.price)}
+                        </span>
+                        /mês durante 1 Ano
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </Link>
-              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div className="flex-1">
-                  <p className="text-sm font-medium text-indigo-600">
-                    {product.category}
-                  </p>
-                  <h5 className="mt-2 text-xl font-semibold text-gray-900">
-                    {product.title}
-                  </h5>
-                  <p
-                    className="mt-3 text-base text-gray-500"
-                    dangerouslySetInnerHTML={{
-                      __html: product.description,
-                    }}
-                  ></p>
-                </div>
-              </div>
-            </div>
-          ))}
+              );
+            })}
+        </div>
         <button
-          onClick={() => setDisplayCount(displayCount + 5)}
+          onClick={() => setDisplayCount(displayCount + 8)}
           className="text-center w-full py-2 bg-indigo-600 hover:bg-indigo-800 text-white"
         >
           Mais Produtos
@@ -252,6 +298,7 @@ const Home = () => {
                     price={item?.price}
                     sold={item?.sold}
                     quantity={item?.quantity}
+                    images={item?.images[0]?.url}
                   />
                 );
               }
@@ -260,74 +307,84 @@ const Home = () => {
         </div>
       </Container>
 
-      <Container class1="popular-wrapper py-5 homer-wrapper-2">
-        <div className="row">
-          <div className="col-12">
-            <h3 className="section-heading">Produtos Populares</h3>
-          </div>
-        </div>
-        <div className="row">
-          {productState &&
-            productState?.map((item, index) => {
-              if (item.tags === "popular") {
-                return (
-                  <div key={index} className={"col-3"}>
-                    <div className="product-card position-relative">
-                      <div className="wishlist-icon position-absolute">
+      <Container class1="popular-wrapper py-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-4">Produtos Populares</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {productState &&
+              productState?.map((item, index) => {
+                const formattedPrice = new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(item.price);
+                if (item.tags === "popular") {
+                  return (
+                    <div
+                      key={index}
+                      className="bg-white rounded-lg shadow-lg p-4"
+                    >
+                      <div className="relative">
                         <button
-                          className="border-0 bg-transparent "
-                          onClick={(_id) => {
-                            addToWishList(item?._id);
-                          }}
+                          className="absolute top-2 right-2 bg-transparent border-none"
+                          onClick={() => addToWishList(item._id)}
                         >
-                          <img src={wish} alt="Produtos" />
+                          <img
+                            src={wish}
+                            alt="Wishlist"
+                            className="h-5 w-5 hover:scale-150 "
+                          />
                         </button>
+                        <Link
+                          to={`/product/${item?._id}`}
+                          className="border-0 bg-transparent"
+                        >
+                          <img
+                            src={
+                              item.images[0].url ? item.images[0].url : watch2
+                            }
+                            alt="Product"
+                            className="w-100 h-auto object-cover"
+                          />
+                        </Link>
                       </div>
-                      <div className="product-image">
-                        <img
-                          src={
-                            item?.images[0].url ? item?.images[0].url : watch2
-                          }
-                          className="img-fluid  mx-auto"
-                          alt="Produtos"
-                          width={160}
-                        />
-                      </div>
-                      <div className="product-details">
-                        <h6 className="brand">{item?.brand}</h6>
-                        <h5 className="product-card">{item?.title}</h5>
+                      <div className="mt-4">
+                        <h6 className="text-lg font-semibold">{item.brand}</h6>
+                        <h5 className="text-xl font-bold mb-2">{item.title}</h5>
                         <ReactStars
                           count={5}
-                          value={item?.totalrating}
                           size={24}
+                          value={item.totalrating}
                           edit={false}
-                          activeColor={`#ffd700`}
+                          activeColor="#ffd700"
                         />
-                        <p className="price">R${item?.price}</p>
+                        <p className="text-lg font-bold text-green-500">
+                          {formattedPrice}
+                        </p>
                       </div>
-                      <div className="action-bar position-absolute">
-                        <div className="d-flex flex-column gap-15">
-                          <button className="border-0 bg-transparent ">
-                            <img src={compare} alt="Adicionar em Carrinho" />
-                          </button>
-                          <button className="border-0 bg-transparent ">
-                            <img
-                              onClick={() => navigate("/product/" + item?._id)}
-                              src={view}
-                              alt="Visualizar"
-                            />
-                          </button>
-                          <button className="border-0 bg-transparent ">
-                            <img src={cart} alt="Adicionar em Carrinho" />
-                          </button>
-                        </div>
+                      <div className="flex justify-end mt-4">
+                        <button className="border-none bg-transparent">
+                          <img
+                            src={compare}
+                            alt="Compare"
+                            className="h-6 w-6 mr-4"
+                          />
+                        </button>
+                        <button
+                          className="border-none bg-transparent"
+                          onClick={() => navigate(`/product/${item._id}`)}
+                        >
+                          <img src={view} alt="View" className="h-6 w-6 mr-4" />
+                        </button>
+                        <button className="border-none bg-transparent">
+                          <img src={cart} alt="Cart" className="h-6 w-6" />
+                        </button>
                       </div>
                     </div>
-                  </div>
-                );
-              }
-              return true;
-            })}
+                  );
+                }
+                return true;
+              })}
+          </div>
         </div>
       </Container>
 
